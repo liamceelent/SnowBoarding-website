b4e491db0d53ddfe37d7c55783408aa88827b180
liam <liamceelent@gmail.com>
2020-04-25 02:17:47 +1200

    made a search bar for gear page plus add a table in database. now working out how to do a filter dropdown menu

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template, request, session
 import sqlite3
 
 app = Flask(__name__)
@@ -7,9 +7,30 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
+
+
 @app.route("/gear")
 def gear_page():
     return render_template('gear.html')
+
+@app.route("/gear", methods = ['POST'])
+def gear_post():
+
+    if request.method == 'POST' and "search_bar" in request.form:
+
+        search = request.form['search_bar']
+
+        conn = sqlite3.connect("snowbaord.db")
+        c = conn.cursor()
+        c.execute("select * from one where name LIKE '%"+ search +"%'")
+
+        result = c.fetchall()
+        conn.close()
+        return render_template('gear.html', tests = result)
+
+    return render_template('gear.html')
+
+
 
 @app.route("/people")
 def people():
@@ -21,13 +42,21 @@ def people():
     conn.close()
     return render_template('people.html', results = result)
 
+
+
+
 @app.route("/event")
 def event():
     return render_template('event.html')
 
+
+
+
 @app.route("/guide")
 def guide():
     return render_template('guide.html')
+
+
 
 
 if __name__ == "__main__":
