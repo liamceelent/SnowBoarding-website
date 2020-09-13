26b8de998644a705b117ff84058f038c15d38cf8
liam <liamceelent@gmail.com>
2020-09-13 18:52:20 +1200

    near final commit :)

--- func.py
+++ func.py
@@ -10,6 +10,15 @@ def database(query):
     conn.close()
     return (result)
 
+def database_var(query, var): # dtabase with variable
+
+    conn = sqlite3.connect("snowbaord.db")
+    c = conn.cursor()
+    c.execute(query,var)
+    result = c.fetchall()
+    conn.close()
+    return (result)
+
 def filterbrand(fcount, bcount, query):
     queryadd = ""
     if fcount > 0:# checking whether another brand has be input into the query
@@ -19,7 +28,6 @@ def filterbrand(fcount, bcount, query):
         if bcount >1: # checking if another brand is in query
             queryadd += "OR " # if another brand has been entered
     queryadd += query # Add Filter
-
     return(queryadd)
 
 def filtercolor(lcount, bcount, ccount, query):
@@ -45,7 +53,384 @@ def filtersize(bcount, ccount, scount, gcount, query) :
             queryadd  += "OR " # Add Filter
     queryadd  += query # Add Filter
 
+def filtersize_noc(bcount, scount, gcount, query) : # no c stands for no colors being filtered
+    queryadd = ""
+    if gcount > 0: # if more than one size has been added to the query
+        if scount > 1: # checking if ther has been more than filter for size
+            queryadd  += "OR " # Add Filter
+    queryadd  += query # Add Filter
+
+    print(queryadd)
     return(queryadd)
+
+
+
+
+
+def filtersnowboots(Burton,DC,Small, Medium, Large,l200,l300,l400,l500,l600):
+
+    bcount = 0 # counting how many brand request forms we have
+    scount = 0 # counting how many size request forms we have
+    pcount = 0 # counting how many price request forms we have
+
+
+    brands = [Burton,DC]
+    for i in range(0,2):
+        if brands[i] is not None:
+            bcount+= 1
+
+    sizes = [Small, Medium, Large]
+    for i in range(0,3):
+        if sizes[i] is not None:
+            scount+= 1
+
+    prices = [l200,l300,l400,l500,l600]
+    for i in range(0,4):
+        if prices[i] is not None:
+            pcount+= 1
+
+    print(scount)
+    print(pcount)
+    print(bcount)
+    fcount = 0 # using this to see how many brands have been placed in the query
+
+    query = "SELECT * FROM snow_boots "
+
+    if bcount or scount or pcount > 0:
+        if fcount == 0:
+            query += "WHERE "
+
+    if bcount > 0:
+        query += "(" # if there is more than one brand to be filtered
+
+    brand = ["'Burton'","'DC'"]
+
+    for i in range(0,2):
+        if brands[i] is not None:
+            query += filterbrand(fcount, bcount, "brand = " + brand[i] +"")
+            fcount += 1
+
+    if bcount > 0 :
+        query += ")"
+
+
+    if bcount  > 0: # checking if and is neccessary and brackets
+            if scount > 0:
+                query += "AND ("
+
+    if bcount == 0:
+        if scount > 0:
+            query+= "("
+
+    gcount = 0
+
+    size = ["'Small'","'Medium'","'Large'"]
+
+    for i in range(0,3):
+        if sizes[i] is not None:
+            query += filtersize_noc(bcount, scount, gcount, "size = " + size[i] +"")
+            gcount += 1
+
+    if scount > 0:
+        query += ")"
+
+    kcount = 0 # counting if there has been a query
+    if pcount > 0:
+        if bcount or scount > 0:
+            query += " AND "
+
+
+    if  l600  is None:
+        if l500 is not None:
+            if kcount == 0:
+                query += "price <= 500"
+                kcount += 1
+
+    if  l600 or l500  is None:
+        if l400 is not None:
+            if kcount ==0:
+                query += "price <= 400"
+                kcount += 1
+
+
+    if  l600 or l500 or l400 is None:
+        if l300 is not None:
+            if kcount == 0:
+                query += "price <= 300"
+                kcount += 1
+
+    if  l600 or l500 or l400 or l300 is None:
+        if l200 is not None:
+            if kcount == 0:
+                query += "price <= 200"
+                kcount += 1
+    print(query)
+    return(query)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def filterclothes(Burton,North_Face,Macpac,Swandry,Kathmandu,Small, Medium, Large,\
+l200,l300,l400,l500,l600):
+
+    bcount = 0 # counting how many brand request forms we have
+    scount = 0 # counting how many size request forms we have
+    pcount = 0 # counting how many price request forms we have
+
+
+    brands = [Burton,North_Face,Macpac,Swandry,Kathmandu]
+    for i in range(0,5):
+        if brands[i] is not None:
+            bcount+= 1
+
+    sizes = [Small, Medium, Large]
+    for i in range(0,3):
+        if sizes[i] is not None:
+            scount+= 1
+
+    prices = [l200,l300,l400,l500,l600]
+    for i in range(0,4):
+        if prices[i] is not None:
+            pcount+= 1
+
+    fcount = 0 # using this to see how many brands have been placed in the query
+
+    query = "SELECT * FROM clothes "
+
+    if bcount or scount or pcount > 0:
+        if fcount == 0:
+            query += "WHERE "
+
+    if bcount > 0:
+        query += "(" # if there is more than one brand to be filtered
+
+    brand = ["'Burton'","'North_Face'","'Macpac'","'Swandry'","'Kathmandu'"]
+
+    for i in range(0,5):
+        if brands[i] is not None:
+            query += filterbrand(fcount, bcount, "brand = " + brand[i] +"")
+            fcount += 1
+
+    if bcount > 0 :
+        query += ")"
+
+
+    if bcount  >= 1: # checking if and is neccessary and brackets
+            if scount > 0:
+                query += "AND ("
+
+    gcount = 0
+
+    size = ["'Small'","'Medium'","'Large'"]
+
+    for i in range(0,3):
+        if sizes[i] is not None:
+            query += filtersize_noc(bcount, scount, gcount, "size = " + size[i] +"")
+            gcount += 1
+
+    print(query)
+
+
+    if bcount >= 1:
+        if scount >0:
+            query += ")"
+
+    kcount = 0 # counting if there has been a query
+    if pcount > 0:
+        if bcount or scount > 0:
+            query += " AND "
+
+
+    if  l600  is None:
+        if l500 is not None:
+            if kcount == 0:
+                query += "price <= 500"
+                kcount += 1
+
+    if  l600 or l500  is None:
+        if l400 is not None:
+            if kcount ==0:
+                query += "price <= 400"
+                kcount += 1
+
+
+    if  l600 or l500 or l400 is None:
+        if l300 is not None:
+            if kcount == 0:
+                query += "price <= 300"
+                kcount += 1
+
+    if  l600 or l500 or l400 or l300 is None:
+        if l200 is not None:
+            if kcount == 0:
+                query += "price <= 200"
+                kcount += 1
+    print(query)
+    return(query)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def filtersnowbinding(Jones_Snowboards,Gnu,Lib_Tech,Salomon,Burton,Small, Medium, Large,\
+l200,l300,l400,l500,l600):
+
+    bcount = 0 # counting how many brand request forms we have
+    scount = 0 # counting how many size request forms we have
+    pcount = 0 # counting how many price request forms we have
+
+
+    brands = [Jones_Snowboards,Gnu,Lib_Tech,Salomon,Burton]
+    for i in range(0,5):
+        if brands[i] is not None:
+            bcount+= 1
+
+    sizes = [Small, Medium, Large]
+    for i in range(0,3):
+        if sizes[i] is not None:
+            scount+= 1
+
+    prices = [l200,l300,l400,l500,l600]
+    for i in range(0,4):
+        if prices[i] is not None:
+            pcount+= 1
+
+    fcount = 0 # using this to see how many brands have been placed in the query
+
+    query = "SELECT * FROM snowbinding "
+
+    if bcount or scount or pcount > 0:
+        if fcount == 0:
+            query += "WHERE "
+
+    if bcount > 0:
+        query += "(" # if there is more than one brand to be filtered
+
+    brand = ["'Jones_Snowboards'", "'Gnu'", "'Lib_Tech'", "'Salomon'", "'Burton'"]
+
+    for i in range(0,5):
+        if brands[i] is not None:
+            query += filterbrand(fcount, bcount, "brand = " + brand[i] +"")
+            fcount += 1
+
+    if bcount > 0 :
+        query += ")"
+
+
+    if bcount  >= 1: # checking if and is neccessary and brackets
+            if scount > 0:
+                query += "AND ("
+
+    gcount = 0
+
+    size = ["'Small'","'Medium'","'Large'"]
+
+    for i in range(0,3):
+        if sizes[i] is not None:
+            query += filtersize_noc(bcount, scount, gcount, "size = " + size[i] +"")
+            gcount += 1
+
+    print(query)
+
+
+    if bcount >= 1:
+        if scount >0:
+            query += ")"
+
+    kcount = 0 # counting if there has been a query
+    if pcount > 0:
+        if bcount or scount > 0:
+            query += " AND "
+
+
+    if  l600  is None:
+        if l500 is not None:
+            if kcount == 0:
+                query += "price <= 500"
+                kcount += 1
+
+    if  l600 or l500  is None:
+        if l400 is not None:
+            if kcount ==0:
+                query += "price <= 400"
+                kcount += 1
+
+
+    if  l600 or l500 or l400 is None:
+        if l300 is not None:
+            if kcount == 0:
+                query += "price <= 300"
+                kcount += 1
+
+    if  l600 or l500 or l400 or l300 is None:
+        if l200 is not None:
+            if kcount == 0:
+                query += "price <= 200"
+                kcount += 1
+
+    return(query)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# snowbaord filterr r wwawf
 
 def filtersnowbaord(Burton,Salomon,Lib_Tech,Jones_Snowboards,Gnu,blue,red,\
 orange,pink,white,black,yellow,other,one_forty,one_forty_two,one_forty_four,\
@@ -56,56 +441,31 @@ one_forty_six,one_forty_eight,l200,l300,l400,l500,l600,g600):
     scount = 0 # counting how many size request forms we have
     pcount = 0 # counting how many price request forms we have
 
-    if Burton is not None:
-        bcount += 1
-    if Salomon is not None:
-        bcount += 1
-    if Lib_Tech is not None:
-        bcount += 1
-    if Jones_Snowboards is not None:
-        bcount += 1
-    if Gnu is not None:
-        bcount += 1
-    #counting the request forms color
-    if blue is not None:
-        ccount += 1
-    if red is not None:
-        ccount += 1
-    if orange is not None:
-        ccount += 1
-    if pink is not None:
-        ccount += 1
-    if white is not None:
-        ccount += 1
-    if black is not None:
-        ccount += 1
-    if yellow is not None:
-        ccount += 1
-    if other is not None:
-        ccount += 1
-    if one_forty is not None:
-        scount += 1
-    if one_forty_two is not None:
-        scount += 1
-    if one_forty_four is not None:
-        scount += 1
-    if one_forty_six is not None:
-        scount += 1
-    if one_forty_eight is not None:
-        scount += 1
-    #counting the request forms price
-    if l200 is not None:
-        pcount += 1
-    if l300 is not None:
-        pcount += 1
-    if l400 is not None:
-        pcount += 1
-    if l500 is not None:
-        pcount += 1
-    if l600 is not None:
-        pcount += 1
-    if g600 is not None:
-        pcount += 1
+    brands = [Jones_Snowboards,Gnu,Lib_Tech,Salomon,Burton]
+    for i in range(0,5):
+        if brands[i] is not None:
+            bcount+= 1
+
+
+    colors = [blue,orange,red,pink,white,black,yellow,other]
+    for i in range(0,6):
+        if colors[i] is not None:
+            ccount+= 1
+
+    sizes = [one_forty,one_forty_two,one_forty_four,one_forty_six,one_forty_eight]
+    for i in range(0,5):
+        if sizes[i] is not None:
+            scount+= 1
+
+    prices = [l200,l300,l400,l500,l600]
+    for i in range(0,4):
+        if prices[i] is not None:
+            pcount+= 1
+
+    print(bcount)
+    print(ccount)
+    print(scount)
+    print(pcount)
 
     # braqndssssssssssssssssssssssssssssssss
 
@@ -117,31 +477,17 @@ one_forty_six,one_forty_eight,l200,l300,l400,l500,l600,g600):
         if fcount == 0:
             query += "WHERE "
 
-    if bcount > 1:
+    if bcount > 0:
         query += "(" # if there is more than one brand to be filtered
 
+    brand = ["'Jones_Snowboards'", "'Gnu'", "'Lib_Tech'", "'Salomon'", "'Burton'"]
 
-    if Burton is not None: # checking if the reqeust form is empty
-        query += filterbrand(fcount,bcount,"brand = 'Burton'")
-        fcount += 1
+    for i in range(0,5):
+        if brands[i] is not None:
+            query += filterbrand(fcount, bcount, "brand = " + brand[i] +"")
+            fcount += 1
 
-    if Salomon is not None:
-        query += filterbrand(fcount,bcount,"brand = 'Salomon'")
-        fcount += 1
-
-    if Lib_Tech is not None:
-        query += filterbrand(fcount,bcount,"brand = 'Lib_Tech'")
-        fcount += 1
-
-    if Jones_Snowboards is not None:
-        query += filterbrand(fcount,bcount,"brand = 'Jones_Snowboards'")
-        fcount += 1
-
-    if Gnu is not None:
-        query += filterbrand(fcount,bcount,"brand = 'Gnu'")
-        fcount += 1
-
-    if bcount >1:
+    if bcount > 0 :
         query += ")"
 
     # colorsssssssssssssssssssssss
@@ -157,7 +503,8 @@ one_forty_six,one_forty_eight,l200,l300,l400,l500,l600,g600):
             query += "(" # if no brand but more that one color
 
 
-    if yellow is not None: # checking weather form is empty or not
+
+    if yellow is not None:
         query += filtercolor(lcount, bcount, ccount,"id =(select snowbaord_id from snowbaord_colour where colour_id =1) ")
         lcount+=1
 
@@ -194,11 +541,9 @@ one_forty_six,one_forty_eight,l200,l300,l400,l500,l600,g600):
         query += filtercolor(lcount, bcount, ccount,"id =(select snowbaord_id from snowbaord_colour where colour_id =6) ")
         lcount+=1
 
-
     if ccount > 1:
         query += ")"
 
-    print(query)#debug
 
     # sizesssssssssssssssssssssssssssssssss
     gcount = 0
@@ -207,25 +552,14 @@ one_forty_six,one_forty_eight,l200,l300,l400,l500,l600,g600):
             if scount > 1:
                 query += "AND ("
 
-    if one_forty is not None: # checking if the form is empty
-        query += filtersize(bcount,ccount,scount,gcount,"size = 140 " )
-        gcount +=1
+    size = ["'140'","'142'","'144'","'146'","'148'"]
 
-    if one_forty_two is not None:
-        query += filtersize(bcount,ccount,scount,gcount,"size = 142 " )
-        gcount +=1
+    for i in range(0,5):
+        if sizes[i] is not None:
+            query += filtersize(bcount, ccount, scount, gcount, "size = " + size[i] +"")
+            gcount += 1
 
-    if one_forty_four is not None:
-        query += filtersize(bcount,ccount,scount,gcount,"size = 144 " )
-        gcount +=1
 
-    if one_forty_six is not None:
-        query += filtersize(bcount,ccount,scount,gcount,"size = 146 " )
-        gcount +=1
-
-    if one_forty_eight is not None:
-        query += filtersize(bcount,ccount,scount,gcount,"size = 148 " )
-        gcount +=1
 
     if bcount or ccount >= 1:
         if scount >1:
@@ -269,15 +603,6 @@ one_forty_six,one_forty_eight,l200,l300,l400,l500,l600,g600):
                 query += "price <= 200"
                 kcount += 1
 
-
-    print(query) #debug
-    print(scount, gcount)#debug
-
-    conn = sqlite3.connect('snowbaord.db')# shorten
-    c = conn.cursor()
-    c.execute(query)
-    test = c.fetchall()
-    conn.close()
 
 
     return(query)
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask, redirect, url_for, render_template, request, session
 import sqlite3
-from func import database, filtersnowbaord
+from func import database, filtersnowbaord, database_var, filtersnowbinding,filterclothes,filtersnowboots
 import os
 import hashlib
 import datetime
@@ -26,7 +26,7 @@ def login():
 @app.route("/login", methods = ['POST', 'GET'])
 def create_acc():
 
-    if request.method == 'POST' and "create_name" in request.form:
+    if request.method == 'POST' and "create_name" in request.form: # seeing if account has been maid
 
         name = request.form['create_name']
         password = request.form['create_pass']
@@ -41,7 +41,7 @@ def create_acc():
         brand.insert(2, key)
         brand = tuple(brand)
 
-        query = "INSERT INTO user(name, salt, key) VALUES(?, ?, ?)"
+        query = "INSERT INTO user(name, salt, key) VALUES(?, ?, ?)" # inserting an account
 
         c = conn.cursor()
         c.execute(query, brand)
@@ -50,7 +50,7 @@ def create_acc():
 
         return render_template('login.html')
 
-    if request.method == 'POST' and "name" in request.form:
+    if request.method == 'POST' and "name" in request.form: # checking if login maths
 
         name = request.form['name']
         password = request.form['pass']
@@ -76,23 +76,53 @@ def create_acc():
                 stat = "wrong user name mate"
                 return render_template('login.html', stat=stat)
 
-@app.route("/gear", methods = ['POST', 'GET'])
+@app.route("/gear", methods = ['POST', 'GET']) # gear page
 def gear_page():
 
     return render_template('gear.html')
 
 @app.route("/gear_snowbinding", methods = ['POST', 'GET'])
 def gear_snowbinding():
+    search_snowbinding = request.form.get('search_bar')
+
+    #brands request
+
+    Burton = request.form.get('Burton')
+    Salomon = request.form.get('Salomon')
+    Lib_Tech = request.form.get('Lib_Tech')
+    Jones_Snowboards = request.form.get('Jones_Snowboards')
+    Gnu = request.form.get('Gnu')
+
+    # sizes "
+
+    Large = request.form.get('Large')
+    Medium = request.form.get('Medium')
+    Small = request.form.get('Small')
+
+    #prices "
+
+    l200 = request.form.get('200')
+    l300 = request.form.get('300')
+    l400 = request.form.get('400')
+    l500 = request.form.get('500')
+    l600 = request.form.get('600')
 
     snowbinding_r = database("select * from snowbinding where id >= 1") # r stands for result
 
-    if request.method == 'POST' and "search_bar" in request.form:
-        search_snowbinding = request.form['search_bar']
+    if search_snowbinding is not None:
+        search_snowbinding = database("select * from snowbinding where name LIKE '%"+ search_snowbinding +"%'")
+        return render_template('gear_snowbinding.html', snowbinding_r = search_snowbinding)
 
-        search_snowbinding_r = database("select * from snowbinding where name LIKE '%"+ search_snowbinding +"%'")
-        # search bar for snowbinding
-        return render_template('gear_snowbinding.html', snowbinding_r = search_snowbinding_r)
-    return render_template('gear_snowbinding.html', snowbinding_r = snowbinding_r)
+    query = filtersnowbinding(Jones_Snowboards,Gnu,Lib_Tech,Salomon,Burton,Small, Medium, Large,\
+    l200,l300,l400,l500,l600) #func filter
+
+    conn = sqlite3.connect('snowbaord.db')
+    c = conn.cursor()
+    c.execute(query)
+    query_r = c.fetchall()
+    conn.close()
+
+    return render_template('gear_snowbinding.html', snowbinding_r = query_r) # page load without search
 
 
 
@@ -112,7 +142,7 @@ def gear_snowbaord():
     Jones_Snowboards = request.form.get('Jones_Snowboards')
     Gnu = request.form.get('Gnu')
 
-    #colour
+    #colour "
     blue = request.form.get('blue')
     red = request.form.get('red')
     orange = request.form.get('orange')
@@ -123,7 +153,7 @@ def gear_snowbaord():
     other = request.form.get('other')
     green = request.form.get('green')
 
-    # sizes
+    # sizes "
 
     one_forty = request.form.get('140')
     one_forty_two = request.form.get('142')
@@ -131,7 +161,7 @@ def gear_snowbaord():
     one_forty_six = request.form.get('146')
     one_forty_eight = request.form.get('148')
 
-    #prices
+    #prices "
 
     l200 = request.form.get('200')
     l300 = request.form.get('300')
@@ -147,7 +177,7 @@ def gear_snowbaord():
 
     query = filtersnowbaord(Burton,Salomon,Lib_Tech,Jones_Snowboards,Gnu,blue,red,\
     orange,pink,white,black,yellow,other,one_forty,one_forty_two,one_forty_four,\
-    one_forty_six,one_forty_eight,l200,l300,l400,l500,l600,g600)
+    one_forty_six,one_forty_eight,l200,l300,l400,l500,l600,g600) #func filter
 
     conn = sqlite3.connect('snowbaord.db')
     c = conn.cursor()
@@ -161,30 +191,81 @@ def gear_snowbaord():
 @app.route("/gear_clothes", methods = ['POST', 'GET'])
 def gear_clothes():
 
-    result = database("select * from clothes where id >= 1")
+    Burton = request.form.get('Burton')
+    North_Face = request.form.get('North_Face')
+    Macpac = request.form.get('Macpac')
+    Swandry = request.form.get('Swandry')
+    Kathmandu = request.form.get('Kathmandu')
+
+    # sizes "
+
+    Large = request.form.get('Large')
+    Medium = request.form.get('Medium')
+    Small = request.form.get('Small')
+
+    #prices "
+
+    l200 = request.form.get('200')
+    l300 = request.form.get('300')
+    l400 = request.form.get('400')
+    l500 = request.form.get('500')
+    l600 = request.form.get('600')
+
 
     if request.method == 'POST' and "search_bar" in request.form:
         search_clothes = request.form['search_bar']
-
-        search_clothes_r  = database("select * from snowbinding where name LIKE '%"+ search_clothes +"%'")
-
+        search_clothes_r  = database("select * from clothes where name LIKE '%"+ search_clothes +"%'")
         return render_template('gear_clothes.html', clothes_r = search_clothes_r)
-    return render_template('gear_clothes.html', clothes_r = result)
+
+    query = filterclothes(Burton,North_Face,Macpac,Swandry,Kathmandu,Small, Medium, Large,\
+    l200,l300,l400,l500,l600)
+
+    conn = sqlite3.connect('snowbaord.db')
+    c = conn.cursor()
+    c.execute(query)
+    query_r = c.fetchall()
+    conn.close()
+
+    return render_template('gear_clothes.html', clothes_r = query_r)
 
 
 
 @app.route("/gear_snow_boots", methods = ['POST', 'GET'])
 def gear_snow_boots():
 
-    result = database("select * from snow_boots where id >= 1")
+    DC = request.form.get('DC')
+    Burton = request.form.get('Burton')
+
+
+    # sizes "
+
+    Large = request.form.get('Large')
+    Medium = request.form.get('Medium')
+    Small = request.form.get('Small')
+
+    #prices "
+
+    l200 = request.form.get('200')
+    l300 = request.form.get('300')
+    l400 = request.form.get('400')
+    l500 = request.form.get('500')
+    l600 = request.form.get('600')
+
 
     if request.method == 'POST' and "search_bar" in request.form:
         search_snowboots = request.form['search_bar']
-
         search_snowboots_r = database("select * from snow_boots where name LIKE '%"+ search_snowboots +"%'")
+        return render_template('gear_snow_boots.html', snowboots_r = search_snowboots_r)
 
-        return render_template('gear_snow_boots.html', snowboots = search_snowboots_r)
-    return render_template('gear_snow_boots.html', snowboots = result)
+    query = filtersnowboots(Burton,DC,Small, Medium, Large,l200,l300,l400,l500,l600)
+
+    conn = sqlite3.connect('snowbaord.db')
+    c = conn.cursor()
+    c.execute(query)
+    query_r = c.fetchall()
+    conn.close()
+
+    return render_template('gear_snow_boots.html', snowboots_r = query_r)
 
 
 @app.route("/gear_click")
@@ -193,19 +274,23 @@ def gear_click():
     search_id = request.args.get('search_id')
     table = request.args.get('table')
 
-    conn = sqlite3.connect("snowbaord.db")# shorten
-    c = conn.cursor()
-    c.execute("select * from snowbaord where id = ?",(search_id,))
-    result = c.fetchall()
-    conn.close()
+    if table == "3":
+        result = database_var("select * from snowbaord where id = ?",(search_id,)) # indivudual gear click for snowbaord
+
+    if table == "4":
+        result = database_var("select * from snowbinding where id = ?",(search_id,)) # " " " "
+
+
+    if table == "2":
+        result = database_var("select * from snow_boots where id = ?",(search_id,)) # " " " "
+
+
+    if table == "1":
+        result = database_var("select * from clothes where id = ?",(search_id,)) # " " " " clothes
+
 
     return render_template('gearspec.html', name = search_id, result = result, table= table)
 
-
-
-@app.route("/people")
-def people():
-    return render_template('people.html')
 
 
 
@@ -213,23 +298,11 @@ def people():
 @app.route("/forms")
 def forms():
 
-    conn = sqlite3.connect("snowbaord.db")# shorten
-    c = conn.cursor()
-    c.execute("select * from formpost")
-    all_forms = c.fetchall()
-    conn.close()
+    all_forms = database("select * from formpost")
 
-    conn = sqlite3.connect("snowbaord.db")# shorten
-    c = conn.cursor()
-    c.execute("select * from user where name =?",(session['username'],))
-    personal_stat = c.fetchall()
-    conn.close()
+    personal_stat = database_var("select * from user where name =?",(session['username'],))
 
-    conn = sqlite3.connect("snowbaord.db") # shorten
-    c = conn.cursor()
-    c.execute("select name from user ORDER BY post desc limit 10")
-    top_posters = c.fetchall()
-    conn.close()
+    top_posters = database("select name from user ORDER BY post desc limit 10")
 
     return render_template('forms.html', all_forms = all_forms, stat = personal_stat, top_posters = top_posters)
 
@@ -247,50 +320,34 @@ def forms_post():
     if request.method == 'POST' and "search_form" in request.form:
         search = request.form['search_form']
 
-    if title is None:# shorten
+    if title is None:
         if content is None:
-            conn = sqlite3.connect("snowbaord.db")
-            c = conn.cursor()
-            c.execute("select * from formpost where title LIKE '%"+ search +"%' OR post LIKE '%"+ search +"%'")
-            result = c.fetchall()
-            conn.close()
+            result = database("select * from formpost where title LIKE '%"+ search +"%' OR post LIKE '%"+ search +"%'")
 
-    if title is None:# shorten
+    if title is None:
         if content is not None:
-            conn = sqlite3.connect("snowbaord.db")
-            c = conn.cursor()
-            c.execute("select * from formpost where post LIKE '%"+ search +"%'")
-            result = c.fetchall()
-            conn.close()
+            result = database("select * from formpost where post LIKE '%"+ search +"%'")
 
-    if title is not  None:# shorten
+    if title is not  None:
         if content is None:
-            conn = sqlite3.connect("snowbaord.db")
-            c = conn.cursor()
-            c.execute("select * from formpost where title LIKE '%"+ search +"%'")
-            result = c.fetchall()
-            conn.close()
+            result = database("select * from formpost where title LIKE '%"+ search +"%'")
 
-    if title is not None:# shorten
+    if title is not None:
         if content is not None:
-            conn = sqlite3.connect("snowbaord.db")
-            c = conn.cursor()
-            c.execute("select * from formpost where title LIKE '%"+ search +"%' OR post LIKE '%"+ search +"%'")
-            result = c.fetchall()
-            conn.close()
+            result = database("select * from formpost where title LIKE '%"+ search +"%' OR post LIKE '%"+ search +"%'")
 
-    conn = sqlite3.connect("snowbaord.db") # shorten
-    c = conn.cursor()
-    c.execute("select * from user where name =?",(session['username'],))
-    personal_stat = c.fetchall()
-    conn.close()
+    personal_stat = database_var("select * from user where name =?",(session['username'],))
 
 
-    return render_template('forms.html', user = session['username'], stat = personal_stat, all_forms = result, top_posters = top_posters,)
+    return render_template('forms.html', user = session['username'], stat = personal_stat, all_forms = result)
 
 @app.route("/forms/create")
 def forms_create():
-    return render_template('create_post.html')
+    if "username" in session:
+        return render_template('create_post.html')
+    else:
+        stat = "please login"
+        return render_template('login.html', stat = stat)
 
 
 @app.route("/forms/create", methods = ['POST', 'GET'])
